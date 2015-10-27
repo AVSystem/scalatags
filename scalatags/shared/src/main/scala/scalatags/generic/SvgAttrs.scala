bfb93e16432a61ba952c39a46a283433465f171d
starzu <m.starzec@avsystem.com>
2015-10-27 11:27:50 +0100

    Svg attrs - namespaces fix

--- scalatags/shared/src/main/scala/scalatags/generic/SvgAttrs.scala
+++ scalatags/shared/src/main/scala/scalatags/generic/SvgAttrs.scala
@@ -1452,7 +1452,7 @@ trait SvgAttrs[Builder, Output <: FragT, FragT] extends Util[Builder, Output, Fr
    *
    * MDN
    */
-  val xLinkHref= "xlink:href".attr
+  val xLinkHref= "xlink:href".nsAttr(Namespace.svgXlinkNamespaceConfig)
 
 
   /*
@@ -1492,7 +1492,7 @@ trait SvgAttrs[Builder, Output <: FragT, FragT] extends Util[Builder, Output, Fr
    *
    * MDN
    */
-  val xmlnsXlink = "xmlns:xlink".nsAttr(Namespace.svgXlinkNamespaceConfig)
+  val xmlnsXlink = "xmlns:xlink".attr
 
 
   /*
